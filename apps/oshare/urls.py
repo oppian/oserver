32edb09c11225a2e84e2cf6b475e71622affdcd6
Neil Davis <neil@oppian.com>
2010-03-12 16:08:16 +0000

    Updated facebook photos template to make it refer to the process as a sync rather than import. Included a few instructions in the template too on expected behaviour. Added ability to switch facebook users

--- apps/oshare/urls.py
+++ apps/oshare/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns("",
     url(r"^fblogin/$", "oshare.views.fblogin", name="oshare_fblogin"),
+    url(r"^fblogout/$", "oshare.views.fblogout", name="oshare_fblogout"),
 )
