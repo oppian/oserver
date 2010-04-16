c84ca249000c280195650e5d8f78bc236cdda8d3
oppianmatt <matt@oppian.com>
2010-04-16 21:10:03 +0100

    removing tag_app from migration script as got no models

--- deploy/db_migrate.sh
+++ deploy/db_migrate.sh
@@ -6,5 +6,4 @@ python manage.py migrate account 0001 --fake
 python manage.py migrate basic_profiles 0001 --fake
 python manage.py migrate oshare 0001 --fake
 python manage.py migrate photos 0001 --fake
-python manage.py migrate tag_app 0001 --fake
 python manage.py migrate tribes 0001 --fake
